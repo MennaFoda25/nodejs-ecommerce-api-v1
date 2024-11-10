d8796ada03a902459e16a1374641a21d51ad2b09
menna <mena.foda2000@gmail.com>
2024-11-10 22:14:09 +0200

    error handling

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require("morgan");
 
 dotenv.config({ path : 'config.env'});
+const ApiError = require("./utils/apiError");
 const dbConnection =require("./config/database")
 
 const categoryRoute = require('./Routes/categoryRoutes')
@@ -25,13 +26,22 @@ if(process.env.NODE_ENV === 'development'){
 app.use('/api/v1/categories', categoryRoute);
 
 app.all('*', (req,res,next)=>{
-    const err = new Error (`Can't find this route ${req.originalUrl}`)
-    next(err.message)
+   // const err = new Error(`Can't find this route ${req.originalUrl}`)
+   // next(err.message)
+
+   next(new ApiError(`Can't find this route ${req.originalUrl}`, 400));
 })
 
 //middleware to handle errors and return them in json 
 app.use((err, req, res, next)=>{
-    res.status(400).json({err})
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack,
+    })
 })
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, ()=>{
